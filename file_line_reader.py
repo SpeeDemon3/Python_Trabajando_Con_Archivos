dfe63bf7b806cabcc14c1d6418bc69a8266f7a5d
Antonio Ruiz <3speedemon3@gmail.com>
2023-10-25 21:38:26 +0200

    Trabajando con archivos locales

--- file_line_reader.py
+++ file_line_reader.py
@@ -2,5 +2,5 @@
 file_name = 'pi_digits.txt'
 
 with open(file_name) as file_object:
-    for line in file_object:
-        print(line)
+    for line in file_object: # Recorremos el archivo linea a linea
+        print(line.rstrip()) # Eliminamos espacios a la derecha
