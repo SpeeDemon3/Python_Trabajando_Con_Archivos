2ce921d5c81b1cbaf9bdfb13fe911369ac4d2c20
Antonio Ruiz <3speedemon3@gmail.com>
2023-10-25 21:53:09 +0200

    Trabajando con el contenido de un archivo

--- work_with_the_content_of_a_file.py
+++ work_with_the_content_of_a_file.py
@@ -3,5 +3,12 @@
 file_name = 'pi_digits.txt'
 
 with open(file_name) as file_object:
-    line = file_object.readline() # El metodo readline() coge cada linea del archivo y la guarda en una lista
+    lines = file_object.readlines() # El metodo readlines() coge cada linea del archivo y la guarda en una lista
 
+pi_str = ''
+
+for line in lines:
+    pi_str += line.strip() # Vamos guardando el contenido eliminando espacios
+
+print(pi_str)
+print(len(pi_str))
