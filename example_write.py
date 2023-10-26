75ba08bbb483a70a23cb476dbf8261aa2b07c6da
Antonio Ruiz <3speedemon3@gmail.com>
2023-10-26 15:55:25 +0200

    Escribiendo en un archivo

--- example_write.py
+++ example_write.py
@@ -3,3 +3,13 @@ ask_name = input("What's your name?")
 
 with open("guest.txt","w") as file_object:
     file_object.write(ask_name)
+
+print("-Guest Book-")
+count_guest = 1
+while count_guest < 6:
+    new_guest = input("What's your name: ")
+    print(f"Welcome {new_guest}")
+    with open("guest_book.txt", "a") as file_object:
+        file_object.write(f"{count_guest}: {new_guest}\n")
+
+    count_guest += 1
